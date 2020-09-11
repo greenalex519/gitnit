ed928961819bf51beda7963b80cbc3169e9c0314
Alex Green <greenalex519@gmail.com>
2020-09-11 19:14:12 +1000

    show error if .git already exists

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+
+const files = require('./lib/files');
+
+clear(); //clears console
+
+console.log( //displays a banner
+    chalk.yellow(
+        figlet.textSync('Gitnit', {horizontalLayout: 'full'})
+    )
+);
+
+//check for existence of .git
+if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a Git repository!'));
+    process.exit();
+};
